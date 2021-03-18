752ea2f2a652ae1111d058fea4339125431f7f04
octopengj <octopengj@google.com>
2021-03-18 20:49:37 +0900

    메인화면구현 1

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import BasketballImage from "./assets/products/basketball1.jpeg";
+import Avatar from "./assets/icons/avatar.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>판매되는 상품들</Text>
+      <View style={styles.productCard}>
+        <View>
+          <Image
+            style={styles.productImage}
+            source={BasketballImage}
+            resizeMode={"contain"}
+          />
+        </View>
+        <View style={styles.productContents}>
+          <Text style={styles.productName}>농구공</Text>
+          <Text style={styles.productPrice}>50000원</Text>
+          <View style={styles.productsFooter}>
+            <View style={styles.productSeller}>
+              <Image style={styles.productAvatar} source={Avatar} />
+              <Text style={styles.productSellerName}>그랩</Text>
+            </View>
+            <Text style={styles.productDate}>3분 전</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,5 +36,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: 32,
+  },
+  productCard: {
+    width: 320,
+    borderColor: "rgb(230,230,230)",
+    borderWidth: 1,
+    borderRadius: 16,
+    backgroundColor: "white",
+  },
+  productImage: {
+    width: "100%",
+    height: 210,
+  },
+  productContents: {
+    padding: 8,
+  },
+  productSeller: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  productAvatar: {
+    width: 24,
+    height: 24,
+  },
+  productsFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 12,
+  },
+  productName: {
+    fontSize: 16,
+  },
+  productPrice: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 8,
+  },
+  productSellerName: {
+    fontSize: 16,
+  },
+  productDate: {
+    fontSize: 16,
   },
 });
